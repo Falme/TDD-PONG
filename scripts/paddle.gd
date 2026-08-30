abddb9c568612b742b00ed72267333562124ff1a
Falme Streamless <falme_streamless@hotmail.com>
2026-08-30 06:15:28 -0300

    paddles now move up and down from input

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -2,11 +2,21 @@ class_name Paddle
 extends Node2D
 
 @export var _velocity: float
+@export_custom(PROPERTY_HINT_INPUT_NAME, "loose_mode") var input_up: String
+@export_custom(PROPERTY_HINT_INPUT_NAME, "loose_mode") var input_down: String
+
+enum Direction {
+	DOWN = 1,
+	UP = -1,
+	}
 
 func _process(delta: float) -> void:
-	up(delta)
+	if Input.is_action_pressed(input_up):
+		move(Direction.UP, delta)
+
+	if Input.is_action_pressed(input_down):
+		move(Direction.DOWN, delta)
 
 
-func up(delta: float = 1.0) -> void:
-	position.y -= _velocity * delta
-
+func move(direction: Direction, delta: float = 1.0) -> void:
+	position.y += (_velocity * direction) * delta
--- tests/unit/test_paddle.gd
+++ tests/unit/test_paddle.gd
@@ -10,18 +10,41 @@ func test_up_should_move_paddle_up() -> void:
 	_paddle._velocity = 2
 
 	for times in range(1,4):
-		_paddle.up()
+		_paddle.move(_paddle.Direction.UP)
 		assert_eq(_paddle.position.y, -(times*2.0), "paddle should have moved up")
 
 
 func test_up_should_move_relative_to_delta_time() -> void:
 	_paddle._velocity = 3
 
-	_paddle.up(1)
+	_paddle.move(_paddle.Direction.UP)
 	assert_eq(_paddle.position.y, -3.0, "paddle should have moved up")
 
-	_paddle.up(0.5)
+	_paddle.move(_paddle.Direction.UP, 0.5)
 	assert_eq(_paddle.position.y, -4.5, "paddle should have moved up")
 
-	_paddle.up(2)
+	_paddle.move(_paddle.Direction.UP, 2)
 	assert_eq(_paddle.position.y, -10.5, "paddle should have moved up")
+
+
+func test_down_should_move_paddle_down() -> void:
+	_paddle._velocity = 4
+	
+	for times in range(1,4):
+		_paddle.move(_paddle.Direction.DOWN)
+		assert_eq(_paddle.position.y, times*4.0, "paddle should have moved down")
+
+
+func test_down_should_move_relative_to_delta_time() -> void:
+	_paddle._velocity = 3
+
+	_paddle.move(_paddle.Direction.DOWN)
+	assert_eq(_paddle.position.y, 3.0, "paddle should have moved down")
+
+	_paddle.move(_paddle.Direction.DOWN, 2)
+	assert_eq(_paddle.position.y, 9.0, "paddle should have moved down")
+
+	_paddle.move(_paddle.Direction.DOWN, 0.5)
+	assert_eq(_paddle.position.y, 10.5, "paddle should have moved down")
+
+
